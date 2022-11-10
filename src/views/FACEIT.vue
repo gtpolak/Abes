2380d6f92e8a1435ef19b6eeb3143cfeccd824ee
Kamil Bania <kamilbania2@gmail.com>
2022-11-10 02:38:55 +0100

    Face it description, styles and mobile layout

--- src/views/FACEIT.vue
+++ src/views/FACEIT.vue
@@ -3,21 +3,28 @@
     <section>
       <main-navbar></main-navbar>
     </section>
-    <section class="section">
-        What is FACEIT? I have no idea
+    <section class="section has-background-black has-text-white has-text-center-desktop  has-border-top-white is-size-4">
+      <div class="columns py-4">
+        <div class="column is-8 is-offset-2 has-text-left-mobile is-mobile">
+          <p class="is-size-3">
+            FACEIT is the leading independent competitive gaming platform for online multiplayer PvP gamers with more than twenty million users.
+          </p>
+          <div class="block"></div>
+        </div>
+      </div>
     </section>
-    <section class="section">
+    <section class="section has-background-black has-text-white">
       <div class="container">
         <div class="columns">
-          <div class="column is-offset-1 is-10">
-            <b-tabs type="is-boxed" position="is-centered" expanded>
-              <b-tab-item label="FACEIT Level Boost">
+          <div class="column is-offset-2-desktop is-8-desktop is-12-mobile gradient-border-faceit">
+            <b-tabs position="is-centered" expanded>
+              <b-tab-item label="FACEIT Level Boost" icon="chevron-triple-up">
                 <faceit-level-form></faceit-level-form>
               </b-tab-item>
-              <b-tab-item label="FACEIT ELO Boost">
+              <b-tab-item label="FACEIT ELO Boost" icon="numeric">
                 <faceit-elo-form></faceit-elo-form>
               </b-tab-item>
-              <b-tab-item label="FACEIT Test Game">
+              <b-tab-item label="FACEIT Test Game" icon="handshake-outline">
                 <faceit-test-game></faceit-test-game>
               </b-tab-item>
             </b-tabs>
@@ -45,6 +52,18 @@ export default {
     "faceit-elo-form": FACEITEloForm,
     "faceit-test-game": FACEITTestGame,
     "main-footer": MainFooter
-  },
+  }
 };
 </script>
+
+<style>
+.has-border-top-white {
+  border-top: solid white 1px;
+}
+
+.gradient-border-faceit {
+  border-width: 5px;
+  border-style: solid;
+  border-image: linear-gradient(to right, rgba(211,103,246,1), rgba(22, 117, 166, 1)) 1;
+}
+</style>
